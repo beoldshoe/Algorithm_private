f79eeedd5210e4f121613f052881936046fc337a
Hyunsoo(two)Kim <110154145+beoldshoe@users.noreply.github.com>
2023-01-23 23:47:47 +0900

    시각 문제
    시각 문제

---
+++ Etc/시각_Mine.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+cnt = 0
+result = 0
+
+for i in range(59) :
+    if i // 10 == 3 :
+        cnt += 1
+    elif i % 10 == 3 :
+        cnt += 1
+    else :
+        continue
+
+for j in range(n+1) :
+    if j % 10 == 3 :
+        result += 60*60
+    else :
+        result += 60*cnt*2 - cnt*cnt
+
+print(result)
