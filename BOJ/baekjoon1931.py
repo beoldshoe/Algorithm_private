418e59cc4faf26298b1326ae2ddc744c0df49d81
Hyunsoo(two)Kim <110154145+beoldshoe@users.noreply.github.com>
2023-01-26 15:30:51 +0900

    BOJ1931 / 실버1
    https://www.acmicpc.net/problem/1931

---
+++ BOJ/baekjoon1931.py
@@ -0,0 +1,21 @@
+N = int(input())
+
+time = list()
+for _ in range(N) :
+    start, end = map(int, input().split())
+    time.append([start,end])
+    
+time = sorted(time, key=lambda a : a[0])
+time = sorted(time, key=lambda a : a[1])
+
+count = 0
+last = 0
+
+for i, j in time :
+    if i>=last :
+        count += 1
+        last = j
+
+print(count)
+
+
