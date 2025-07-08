074c010179d04ce5fbd1920d84e3e6c725025d75
Hyunsoo KIM <110154145+beoldshoe@users.noreply.github.com>
2025-07-08 22:53:03 +0900

    전화번호목록 / Lv2 / Python
    https://school.programmers.co.kr/learn/courses/30/lessons/42577
    전화번호목록 / Lv2 / Python

---
+++ Programmers/PRGM_전화번호목록.py
@@ -0,0 +1,17 @@
+# startswith 함수 사용
+
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]) :
+            return False
+    return True
+
+# 슬라이싱 사용
+
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            return False
+    return True
