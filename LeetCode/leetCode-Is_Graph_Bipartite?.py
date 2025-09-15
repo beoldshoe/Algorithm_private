3770a136b96c6b8cf2dd730484e1a0e66f1c926c
Hyunsoo KIM <110154145+beoldshoe@users.noreply.github.com>
2025-09-15 22:13:21 +0900

    LeetCode : Is_Graph_Bipartite? / Medium
    https://leetcode.com/problems/coin-change/description/
    LeetCode : Coin_Change / Medium

---
+++ LeetCode/leetCode-Is_Graph_Bipartite?.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        color = [0] * n   # 0: 미방문, 1/-1: 두 색
+
+        for i in range(n):
+            if color[i] != 0:
+                continue
+            # BFS 시작
+            queue = deque([i])
+            color[i] = 1
+            while queue:
+                u = queue.popleft()
+                for v in graph[u]:
+                    if color[v] == 0:  # 아직 안 칠했으면 반대 색
+                        color[v] = -color[u]
+                        queue.append(v)
+                    elif color[v] == color[u]:  # 같은 색이면 이분 그래프 아님
+                        return False
+        return True
